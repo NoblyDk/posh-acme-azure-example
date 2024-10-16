f3f5adc757e282b9986a7a29f2f9749086ad6e5d
Paw Ormstrup Madsen <pm@nobly.dk>
2024-10-16 13:09:50 +0200

    Update Import-AcmeCertificateToKeyVault.ps1

--- Import-AcmeCertificateToKeyVault.ps1
+++ Import-AcmeCertificateToKeyVault.ps1
@@ -3,41 +3,54 @@ param (
     [string] $KeyVaultResourceId
 )
 
-# Split certificate names by comma or semi-colon
+Write-Host "Split certificate names by comma or semi-colon"
 $currentServerName = ((Get-PAServer).Name)
-# For wildcard certificates, Posh-ACME replaces * with ! in the directory name
+$currentServerName
+Write-Host "For wildcard certificates, Posh-ACME replaces * with ! in the directory name"
 $certificateName = $certificateName.Replace('*', '!')
+$certificateName
 
-# Set working directory
+Write-Host "Set working directory"
 $workingDirectory = Join-Path -Path "." -ChildPath "pa"
+$workingDirectory
 
-# Set Posh-ACME working directory
+Write-Host "Set Posh-ACME working directory"
 $env:POSHACME_HOME = $workingDirectory
 Import-Module -Name Posh-ACME -Force
 
-# Resolve the details of the certificate
+Write-Host "Resolve the details of the certificate"
 $currentServerName = ((Get-PAServer).location) -split "/" | Where-Object -FilterScript { $_ } | Select-Object -Skip 1 -First 1
+$currentServerName
 $currentAccountName = (Get-PAAccount).id
+$currentAccountName
 
-# Determine paths to resources
+Write-Host "Determine paths to resources"
 $orderDirectoryPath = Join-Path -Path $workingDirectory -ChildPath $currentServerName | Join-Path -ChildPath $currentAccountName | Join-Path -ChildPath $certificateName
+$orderDirectoryPath
 $orderDataPath = Join-Path -Path $orderDirectoryPath -ChildPath "order.json"
+$orderDataPath
 $pfxFilePath = Join-Path -Path $orderDirectoryPath -ChildPath "fullchain.pfx"
+$pfxFilePath
 
-# If we have a order and certificate available
+Write-Host "If we have a order and certificate available"
 if ((Test-Path -Path $orderDirectoryPath) -and (Test-Path -Path $orderDataPath) -and (Test-Path -Path $pfxFilePath)) {
 
     $pfxPass = (Get-PAOrder $certificateName).PfxPass
     $securePfxPass = ConvertTo-SecureString $pfxPass -AsPlainText -Force
-    # Load PFX
+    Write-Host "Load PFX"
     $certificate = Get-PfxCertificate $pfxFilePath -Password $securePfxPass
-
-    # Get the current certificate from key vault (if any)
+    $certificate
+    
+    Write-Host "Get the current certificate from key vault (if any)"
     $azureKeyVaultCertificateName = $certificateName.Replace(".", "-").Replace("!", "wildcard")
     $keyVaultResource = Get-AzResource -ResourceId $KeyVaultResourceId
+    $keyVaultResource
     $azureKeyVaultCertificate = Get-AzKeyVaultCertificate -VaultName $keyVaultResource.Name -Name $azureKeyVaultCertificateName -ErrorAction SilentlyContinue
-
-    # If we have a different certificate, import it
+    
+    $azureKeyVaultCertificate.Thumbprint
+    $certificate.Thumbprint
+    
+    Write-Host "If we have a different certificate, import it"
     If (-not $azureKeyVaultCertificate -or $azureKeyVaultCertificate.Thumbprint -ne $certificate.Thumbprint) {
         Import-AzKeyVaultCertificate -VaultName $keyVaultResource.Name -Name $azureKeyVaultCertificateName -FilePath $pfxFilePath -Password $securePfxPass | Out-Null    
     }
